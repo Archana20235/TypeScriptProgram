90bea7da3ab9ad55b824542925281f7d59ff84c5
Archana20235 <archanamohini23@gmail.com>
2026-06-29 00:23:49 +0530

    Add Vowlecount files

---
+++ Vowlecount.ts
@@ -0,0 +1,20 @@
+
+let str: string = "Archana Mohini";
+
+let vowels = 0;
+let consonants = 0;
+
+for (let i = 0; i < str.length; i++) {
+    let ch = str[i].toLowerCase();
+
+    if (ch >= 'a' && ch <= 'z') {
+        if (ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u') {
+            vowels++;
+        } else {
+            consonants++;
+        }
+    }
+}
+
+console.log("Vowels =", vowels);
+console.log("Consonants =", consonants);
