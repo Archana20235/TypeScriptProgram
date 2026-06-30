6953ca53104bd30e10e2d9bc576efb8221143413
Archana20235 <archanamohini23@gmail.com>
2026-07-01 00:36:16 +0530

    Commit changes

---
+++ DuplicateChar.ts
@@ -0,0 +1,21 @@
+let Str : string = "Archana Mohini";
+Str = Str.toLowerCase();
+
+let duplicateChar = new Set<String>();
+
+for(let ch of Str){
+
+if(/[a-z]/.test(ch)){
+
+duplicateChar.add(ch);
+}
+}
+
+let result = "";
+
+for(let ch of duplicateChar){
+
+result +=ch;
+
+}
+console.log(result);
